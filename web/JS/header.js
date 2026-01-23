1cd4fee557cd2ec0aae0077aaa8c5b52890c1667
Izan López Barrio <ilopezb05@larioja.edu.es>
2026-01-23 12:44:15 +0100

    Prueba Página Principal

---
+++ web/JS/header.js
@@ -0,0 +1,17 @@
+const menu = document.getElementById("menu")
+const logIn = document.getElementById("botonPrincipal")
+
+menu.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    if (menu.innerHTML == "=") {
+        menu.innerHTML = "x"
+    } else {
+        menu.innerHTML= "="
+    }
+})
+
+logIn.style.cursor = "pointer";
+logIn.addEventListener("click", () => {
+    alert("¡Estamos trabajando en ello!")
+})
